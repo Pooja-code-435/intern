d006cd3c3c07e16135ae5b094ad127be1bb78ba8
Pooja-code-435 <149855057+Pooja-code-435@users.noreply.github.com>
2024-06-13 19:04:12 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,22 @@
+a="pooja"
+for i in a:
+    print(i)
+print(len(a))
+
+a="welcome to training"
+print("welcome" in a)
+
+x="welcome to SDM"
+print(x[-6:-1])
+
+b="pooja"
+c="POOJA"
+print(b.upper())
+print(c.lower())
+
+a="pooja ganesh naik"
+print(a.strip())
+print(a.replace("a","p"))
+
+txt="my name is \"pooja\" ganesh naik"
+print(txt)
